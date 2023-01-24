7ed15737cff6d67040bf0d933e49bc4592ff60c1
AspaNeuro <a.almeida@neurogears.org>
2023-01-24 16:44:09 +0000

    Bonsai Matlab OSC test

---
+++ MatlabLib/osctest.m
@@ -0,0 +1,14 @@
+metadata = sprintf('%s_S1', datetime(datenum(datetime('now')),'Format','yyyy-MM-dd_HH-mm-ss','convertFrom','datenum'));
+osc = OscTcp('127.0.0.1', 4002);
+%osc = OscUdp('127.0.0.1',4002,4007);
+
+
+while(true)
+    datagram = [];
+    while(isempty(datagram))
+        datagram = osc.receive();
+    end
+    disp(datagram);
+    
+end
+osc.delete();
